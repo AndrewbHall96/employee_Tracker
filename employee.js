7658f2cedf67953b13a539b2d10cdbf61e67bdec
Andrew Hall <andrewbhall96@gmail.com>
2020-07-10 22:17:58 -0400

    updating inquirer.prompt

--- employee.js
+++ employee.js
@@ -3,7 +3,7 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    
+
     port: 3306,
 
     user: "root",
@@ -12,9 +12,28 @@ var connection = mysql.createConnection({
     database: "employee_db"
 });
 
-connection.connect(function(err) {
+
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-  
-    // createEmployee();
-  });
+
+});
+
+mainMenu()
+
+function mainMenu() {
+    inquirer.prompt([
+        {
+            message: "What would you like to do?",
+            name: "menuChoice",
+            type: "list",
+            choices: ["View All Employees", "View All EMployees By Department", "View All EMployees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manger"]
+        }
+    ]).then(function (answer) {
+        switch (answer.myChoice) {
+            case "View All Employees":
+                console.log(answer.menuChoice);
+                break;
+        }
+    })
+};
